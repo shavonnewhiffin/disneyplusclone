e222f62344628daf27e68dc7c05ae42cee56561b
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-03-20 12:47:50 -0700

    changed shadow color

--- src/Components/ProductionHouse.jsx
+++ src/Components/ProductionHouse.jsx
@@ -37,7 +37,7 @@ const ProductionHouse = () => {
     <div className="flex items-center justify-center gap-2 md:gap-5 p-2 px-5 md:px-16 sm:w-[550px] md:w-auto sm:m-auto">
         {/* Map over every object in the array and dynamically render image and video */}
         {productionHouseList.map((item)=>(
-        <div className="group relative border-[2px] border-gray-600 rounded-lg hover:scale-110 transitional-all duration-300 ease-in-out cursor-pointer shadow-xl shadow-black">
+        <div className="group relative border-[2px] border-gray-600 rounded-lg hover:scale-110 transitional-all duration-300 ease-in-out cursor-pointer shadow-xl  shadow-gray-800">
 
          <video src={item.video} autoPlay loop playsInline muted className='absolute top-0 rounded-md z-10 opacity-0 group-hover:opacity-50 w-full h-full'/>
 
